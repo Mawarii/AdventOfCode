7069ca0e47d15c3ababc527eb6c83b164461071b
Mawarii <dominik@s-kapi.com>
2022-12-04 11:20:50 +0100

    Update: using sets and build-in issubset function

--- 4/4-1.py
+++ 4/4-1.py
@@ -3,15 +3,10 @@ with open("./4/input") as f:
 
     counter = 0
     for line in lines:
-        sectionOneStart = int(line.split(',')[0].split('-')[0])
-        sectionOneEnd = int(line.split(',')[0].split('-')[1])
+        sectionOne = set(list(range(int(line.split(',')[0].split('-')[0]),int(line.split(',')[0].split('-')[1]) + 1)))
+        sectionTwo = set(list(range(int(line.split(',')[1].split('-')[0]), int(line.split(',')[1].split('-')[1]) + 1)))
 
-        sectionTwoStart = int(line.split(',')[1].split('-')[0])
-        sectionTwoEnd = int(line.split(',')[1].split('-')[1])
-
-        if sectionOneStart <= sectionTwoStart and sectionOneEnd >= sectionTwoEnd:
-            counter += 1
-        elif sectionTwoStart <= sectionOneStart and sectionTwoEnd >= sectionOneEnd:
+        if sectionOne.issubset(sectionTwo) or sectionTwo.issubset(sectionOne):
             counter += 1
 
 print(counter)
