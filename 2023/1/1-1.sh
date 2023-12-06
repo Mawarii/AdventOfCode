0ac4b30d2f945a114c999b74cb0dc1877ef7abf1
Mawari <mawari@mawari.eu>
2023-12-06 21:18:40 +0100

    add: 1-1

--- 2023/1/1-1.sh
+++ 2023/1/1-1.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+main() {
+    sum=0
+
+    input="/home/mawari/Desktop/git-projects/private/AdventOfCode/2023/1/input"
+
+    while IFS= read -r line; do
+        number=$(printf "${line}" | grep -oE '[0-9]' | head -1 | tr -d '\n'; printf "${line}" | grep -oE '[0-9]' | tail -1)
+        sum=$(("${number}"+"${sum}"))
+    done < "$input"
+
+    echo "${sum}"
+}
+
+main
